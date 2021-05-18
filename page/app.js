e387a1fd99a16ff08ae1e70c4501efdeba4c08ab
Daniel-Bichof <Daniel-Bichof@gmail.com>
2021-05-18 15:30:29 -0300

    timer and buttons

--- page/app.js
+++ page/app.js
@@ -1,30 +1,31 @@
 
 function relogio() {
-    let data = new Date()
-    let h = data.getHours()
-    let min =  data.getMinutes()
-    let seg = data.getSeconds()
-
+    let data = new Date();
+    let h = data.getHours();
+    let min =  data.getMinutes();
+    let seg = data.getSeconds();
 
     if( h < 10){
         h = "0"+ h
-    }
+    };
+
     if( min < 10){
         min = "0"+ min
-    }
+    };
+    
     if( seg < 10){
         seg = "0"+ seg
-    }
+    };
     
     let horas = `${h}:${min}:${seg}`
-    //o valor .value do relogio vai receber as horas formatada da lina acima
+    //o valor .value do relógio vai receber as horas formatada da lina acima
     // document.querySelector("#relogio").value = horas; 
 
-    var naTela = document.querySelector(".horario")
-    naTela.innerHTML = `${horas}` 
+    var naTela = document.querySelector(".horario");
+    naTela.innerHTML = `${horas}`;
 
     //console.log(h + ":" + min + ":" + seg);
 
 }
 
-var tempo = setInterval(relogio, 1000)
+var tempo = setInterval(relogio, 1000);
--- page/crono.js
+++ page/crono.js
@@ -1,45 +1,34 @@
-var voltas = []
+var voltas = [];
 
-var h = 0
-var min  = 0
-var sec = 0
-var mSec = 0
+var h = 0;
+var min  = 0;
+var sec = 0;
+var mSec = 0;
 
-var tempo = 10
+var tempo = 10;
 var cron;
-
-function TimeMarker(){
-    var time = document.querySelector('#counter').innerHTML
-    var split = document.querySelector('.time-marker')
-    voltas.push(time)
-    console.log(voltas)
-    console.log(time)
-    split.innerHTML += `${time} <br/>`
-}
 
 function start() {
     pause();
-    cron = setInterval(() => { timer() }, tempo)
-   
+    cron = setInterval(() => { timer() }, tempo);
 }
 
 function pause() {
-    clearInterval(cron)
+    clearInterval(cron);
 }
 
 function stopTimer() {
-    clearInterval(cron)
-    h = 0
-    min = 0
-    sec = 0
-    mSec = 0
-    var ListTime = document.querySelector('.time-marker')
-    ListTime.innerHTML = ''
-
-    document.getElementById('counter').innerHTML = '00:00:00:00'
+    clearInterval(cron);
+    h = 0;
+    min = 0;
+    sec = 0;
+    mSec = 0;
+    var ListTime = document.querySelector('.time-marker');
+    ListTime.innerHTML = '';
+    document.getElementById('counter').innerHTML = '00:00:00:00';
 }
-function timer() {
 
+function timer() {
     mSec++
     if(mSec == 100) {
         mSec = 0
@@ -53,7 +42,6 @@ function timer() {
             }
         }
     }
-    
 
     var format = (h > 10 ? '0' + h : h) + ':' + (min < 10 ? '0' + min : min) + ':' + (sec < 10 ? '0'+ sec: sec) + ':' + (mSec < 10 ? '0'+ mSec : mSec)
     document.getElementById('counter').innerHTML = format
